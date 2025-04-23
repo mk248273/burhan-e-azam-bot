af60f3b30571f6735e8612b0f4cf83387dd6883c
muzammil7ctech <mkn144248273@gmail.com>
2025-04-24 00:07:24 +0500

    code update

--- flask/app.py
+++ flask/app.py
@@ -188,4 +188,4 @@ def init_dummy_data():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port=7888)
+    app.run(debug=True, host='0.0.0.0')
